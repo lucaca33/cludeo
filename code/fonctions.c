ddaa8a8d6d62191f29cde671cdb49e7c03489239
Loucas24 <luk.verron24@gmail.com>
2026-05-13 10:51:44 +0200

    Update fonctions.c

--- code/fonctions.c
+++ code/fonctions.c
@@ -1,1 +1,18 @@
 #include "cluedo.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void lancer_des(Joueur * player)
+{
+    if(player == NULL)
+    {
+        return NULL;
+    }
+    int nombre = 0;
+    const int MIN = 2, MAX = 12;
+    srand(time(NULL));
+    nombre = (rand() % (MAX + 1 - MIN)) + MIN;
+    printf("Vous avez obtenu %d\n", nombre);
+    player->val_des = nombre;
+}
